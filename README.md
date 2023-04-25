d8e924802b0c05a0adf0659a714ab2b9d293922f
Vanessa Rubiera <83053924+VanessaRubiera@users.noreply.github.com>
2023-04-24 20:17:28 -0700

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# GIT CONFLICTS
+
+`Repaso:` Git tiene 3 areas, Working Area, Staging Area y Commit Area.
+
+```git
+git add .
+```
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+#Ctrl + Shift + P = Shortcut Para configuraciones
+name = "Profe de Python"
+message = f"Hola {name}" 
+print(message) #Podemos tener comillas '', "" o """
